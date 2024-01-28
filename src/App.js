16685765ed2d2f4fbb3bb1d213c8611908be38b4
LAPTOP-L74LB9B0\TM <tamanash1997@gmail.com>
2024-01-28 19:21:18 +0530

    Changes for Main Section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import PizzaCard from './Components/pizzaCard';
 
 function App() {
   const [orders, setOrders] = useState([]);
+  const status = {
+    0: "Order Placed",
+    1: "Order in Making",
+    2: "Order Ready",
+    3: "Order Picked"
+  }
 
   function testOrder(){
     const order = {
       id: Math.random() * (25 - 1) + 1,
       status: 0,
-      base: "Large"
+      type: "Veg",
+      size: "Large",
+      base: "Thick",
+      placedAt: Date.now()
     }
 
     let newOrders = [...orders];
@@ -32,11 +41,17 @@ function App() {
       setOrders(currentOrders);
   }
 
+  function getTimeElapsed(){
+
+  }
+
   return (
     <div className="App">
       <button onClick={testOrder}>Add Order</button>
-      <div className='container'>
-        <div className='orders-created'>
+      <h2 className='section-header'>Pizza Stages</h2>
+      <div className='pizza-stages'>
+        <div className='orders-placed'>
+          <h2>Order Placed</h2>
         {
           orders.map((order)=>{
             if(order.status === 0){
@@ -48,6 +63,7 @@ function App() {
         }
         </div>
         <div className='orders-making'>
+          <h2>Order in Making</h2>
         {
           orders.map((order)=>{
             if(order.status === 1){
@@ -59,6 +75,7 @@ function App() {
         }
         </div>
         <div className='orders-ready'>
+          <h2>Order Ready</h2>
         {
           orders.map((order)=>{
             if(order.status === 2){
@@ -69,7 +86,8 @@ function App() {
           })
         }
         </div>
-        <div className='orders-delivered'>
+        <div className='orders-picked'>
+          <h2>Order Picked</h2>
         {
           orders.map((order)=>{
             if(order.status === 3){
@@ -80,6 +98,27 @@ function App() {
           })
         }
         </div>
+      </div>
+      <h2 className='section-header'>Main Section</h2>
+      <div className='main-section'>
+        <div className='header-container'>
+          <div className='main-head main-data'>Order ID</div>
+          <div className='main-head main-data'>Stage</div>
+          <div className='main-head main-data'>Total Time Spent</div>
+          <div className='main-head main-data'>Action</div>
+        </div>
+        {
+          orders.map((order)=>{
+            return (
+              <div className='data-container'>
+                <div className='main-data'>Order ID: {order.id}</div>
+                <div className='main-data'>{status[order.status]}</div>
+                <div className='main-data'>{order.placedAt}</div>
+                <div className='main-data'>{status[order.status]}</div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
